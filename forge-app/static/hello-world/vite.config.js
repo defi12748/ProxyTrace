4e7dcec0ccb3e03d287ab51880a8d7340a41019d
yassinekolsi <yassine.kolsi@gmail.com>
2026-06-19 11:50:37 +0100

    integrate forge-app

---
+++ forge-app/static/hello-world/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+export default defineConfig({
+    base: "./",
+    plugins: [react()],
+    resolve: {
+        dedupe: ["react", "react-dom"]
+    },
+    build: {
+        outDir: "build"
+    },
+    server: {
+        port: 5173
+    }
+});
---
+++ forge-app/static/hello-world/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  base: "./",
+  plugins: [react()],
+  resolve: {
+    dedupe: ["react", "react-dom"]
+  },
+  build: {
+    outDir: "build"
+  },
+  server: {
+    port: 5173
+  }
+});
